575081ade8bd68b3cbd7385b0579dadfbc1cef65
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2021-04-22 00:47:38 +0530

    Create Convert a given roman number to integer.java

---
+++ strings/Convert a given roman number to integer.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+    public static void main(String args[])throws Exception
+    {
+        BufferedReader bu=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        String s=bu.readLine().trim();
+        int val=0,i;
+        for(i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='I') val++;
+            if(s.charAt(i)=='V') val+=5;
+            if(s.charAt(i)=='X') val+=10;
+            if(s.charAt(i)=='L') val+=50;
+            if(s.charAt(i)=='C') val+=100;
+            if(s.charAt(i)=='D') val+=500;
+            if(s.charAt(i)=='M') val+=1000;
+        }
+
+        if(s.contains("IV")) val-=2;
+        if(s.contains("IX")) val-=2;
+        if(s.contains("XL")) val-=20;
+        if(s.contains("XC")) val-=20;
+        if(s.contains("CD")) val-=200;
+        if(s.contains("CM")) val-=200;
+
+        System.out.println(val);
+    }
+}
